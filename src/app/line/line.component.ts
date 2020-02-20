96e60e2ef55e1953f4fd7f9abdfb8f6dff669a42
loukas.kotas <loukas.kotas@gmail.com>
2020-02-21 00:34:19 +0200

    WIP: multiple lines appear. Axis dont resize.

--- src/app/line/line.component.ts
+++ src/app/line/line.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input, ViewEncapsulation, ChangeDetectionStrategy, ChangeDetectorRef, ViewChild, ElementRef } from '@angular/core';
 import * as d3 from 'd3';
 import { LineService } from './services/line.service';
+import { fromEvent, interval } from 'rxjs';
+import { debounce } from 'rxjs/operators';
 
 export interface Line {
   labels: any[];
@@ -50,9 +52,28 @@ export class LineComponent implements OnInit {
 
   ngOnInit() {
 
+    this.generateGraph();
+
+
+    fromEvent(window, 'resize')
+    .pipe(
+      debounce(() => interval(300))
+    )
+    .subscribe(d => {
+      console.log(window.innerWidth);
+      this.options.width = window.innerWidth;
+      this.options.height = window.innerHeight;
+      this.generateGraph();
+
+    });
+
+    // Call the resize function
+
+  }
+
+  private generateGraph() {
     const options = { width: this.options.width - 200, height: this.options.height - 100, margin: this.options.margin };
     this.graph = this.lineService.factoryLineGraph(this.labels, this.data, options);
-
     const pathData = [];
     for (let index = 0; index < this.data.length; index++) {
       pathData.push({
@@ -60,16 +81,13 @@ export class LineComponent implements OnInit {
         y: this.data[index]
       });
     }
-
     // append line. D3 returns line generator, not path data.
     d3.select('#svg-container')
-    .append('path')
-    .attr('class', 'line') // Assign a class for styling
-    .data(
-      [pathData]
-    )
-    .attr('d', this.graph.path);
-
+      .append('path')
+      .attr('class', 'line') // Assign a class for styling
+      .data([pathData])
+      .attr('d', this.graph.path);
+    return options;
   }
 
   translate(x, y) {
