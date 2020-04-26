e10bb0909629448b716f5517b5eb67d8a7eee4aa
loukas.kotas <loukas.kotas@gmail.com>
2020-04-26 20:24:20 +0300

    feat(multiarea): init module, component
    fix #122

---
+++ src/app/components/multiarea/multiarea.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ng-multiarea',
+  templateUrl: './multiarea.component.html',
+  styleUrls: ['./multiarea.component.scss']
+})
+export class MultiareaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/multiarea/multiarea.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MultiareaComponent } from './multiarea.component';
+
+
+
+@NgModule({
+  declarations: [MultiareaComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class MultiareaModule { }
