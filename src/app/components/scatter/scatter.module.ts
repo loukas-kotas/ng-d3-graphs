dbeeb58f460f90a80f41e21d657c147e8168680b
loukas.kotas <loukas.kotas@gmail.com>
2020-05-02 22:44:01 +0300

    WIP: SNAPSHOT SAVED

---
+++ src/app/components/scatter/scatter.module.ts
@@ -0,0 +1,16 @@
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+
+import {ScatterComponent} from './scatter.component';
+
+
+
+@NgModule({
+  declarations: [ScatterComponent],
+  imports: [CommonModule],
+  exports: [
+    ScatterComponent,
+  ]
+})
+export class ScatterModule {
+}
